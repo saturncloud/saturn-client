d38cb004b2f6e3c17abe7c4e5adbcd2718c0b750
bhperry <ben@saturncloud.io>
2023-10-10 23:04:21 +0000

    update docstrings on cli commands with more info

--- saturn_client/cli/commands.py
+++ saturn_client/cli/commands.py
@@ -20,13 +20,16 @@ def cli():
     required=False,
     help="Resource owner name. Defaults to current auth identity.",
 )
-def list(resource_type: Optional[str], resource_name: Optional[str] = None, owner: str = None):
+def resources(resource_type: Optional[str], resource_name: Optional[str] = None, owner: str = None):
     """
-    List resources belonging to an owner. Resources are prefix
-    matched against RESOURCE_NAME, if set.
-    Pass "all" into RESOURCE_NAME to list unfiltered by type (default)
+    List resources belonging to an owner.
 
-    Resource Types: [workspace, deployment, job]
+    \b
+    RESOURCE_TYPE (optional):
+        all: List resources of any type (Default)
+        deployment, job, workspace: Filter for resources of a single type
+    RESOURCE_NAME (optional):
+        Filter results by prefix match on name
     """
     client = SaturnConnection()
     if resource_type == "all":
@@ -41,9 +44,13 @@ def list(resource_type: Optional[str], resource_name: Optional[str] = None, owne
 @click.option("--owner", default=None, required=False)
 def pods(resource_type: str, resource_name: str, owner: str = None):
     """
-    List pods associated with a resource.
+    List active pods associated with a resource.
 
-    Resource Types: [workspace, deployment, job]
+    \b
+    RESOURCE_TYPE (required):
+        deployment, job, workspace
+    RESOURCE_NAME (required):
+        Exact match on name
     """
     client = SaturnConnection()
     resource_type = ResourceType.lookup(resource_type)
@@ -61,23 +68,55 @@ def pods(resource_type: str, resource_name: str, owner: str = None):
     required=False,
     help="Resource owner name. Defaults to current auth identity.",
 )
-def logs(resource_type: str, resource_name: str, owner: str = None, pod_name: str = None):
+@click.option(
+    "-a",
+    "--all-containers",
+    default=False,
+    is_flag=True,
+    help=(
+        "Print logs for all containers. "
+        "By default, only logs for the main container are shown (or init containers during setup)"
+    ),
+)
+def logs(
+    resource_type: str,
+    resource_name: str,
+    owner: str = None,
+    pod_name: str = None,
+    all_containers: bool = False,
+):
     """
     Print resource logs. Defaults to the most recently created pod if POD_NAME is not given.
 
-    Resource Types: [workspace, deployment, job]
+    \b
+    RESOURCE_TYPE (required):
+        deployment, job, workspace
+    RESOURCE_NAME (required):
+        Exact match on name
+    POD_NAME (optional):
+        Fetch logs for a specific pod on the resource. Defaults to the latest pod.
     """
     client = SaturnConnection()
-    logs = client.get_logs(resource_type, resource_name, owner_name=owner, pod_name=pod_name)
+    logs = client.get_logs(
+        resource_type,
+        resource_name,
+        owner_name=owner,
+        pod_name=pod_name,
+        all_containers=all_containers,
+    )
     click.echo(logs)
 
 
 @cli.command()
 @click.argument("input-file")
-@click.option("--start", is_flag=True)
+@click.option("--start", is_flag=True, help="Start the resource after creating/updating it with the recipe")
 def apply(input_file: str, start: bool = False):
     """
     Create or update the contents of a resource recipe.
+
+    \b
+    INPUT_FILE (required):
+        Path to a YAML or JSON recipe file
     """
     with open(input_file, "r") as f:
         yaml = YAML()
