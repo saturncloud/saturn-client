a066fdbafaacba02c839e1c44258e32a0875ba06
hhuuggoo <hugo@saturncloud.io>
2024-03-15 14:29:40 -0700

    Update commands.py
    accidentally called append 2x

--- saturn_client/cli/commands.py
+++ saturn_client/cli/commands.py
@@ -262,7 +262,6 @@ def apply(input_file: str, start: bool = False, sync: List[str] = []):
         commands.append(cmd)
         cmd = f"mkdir -p {dest}"
         commands.append(cmd)
-        commands.append(cmd)
         cmd = f"tar -xvzf /tmp/data.tar.gz -C {dest}"
         commands.append(cmd)
     start_script = recipe["spec"].get("start_script", "")
