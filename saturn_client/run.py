514fa354f3af9e24b364194fb10e64af3263cc24
hugo <hugo@saturncloud.io>
2024-03-15 17:05:35 +0000

    handle batching

--- saturn_client/run.py
+++ saturn_client/run.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+import time
 
 from os.path import join, exists
 import os
@@ -13,7 +14,6 @@ from saturnfs.client.saturnfs import _rsync
 def run_command(cmd: str) -> None:
     output_path = os.environ["SATURN_RUN_REMOTE_OUTPUT_PATH"]
     local_results_dir = os.environ["SATURN_RUN_LOCAL_RESULTS_DIR"]
-    print("YAAAAY", output_path, local_results_dir)
     remote_results_dir = join(output_path, "results")
     remote_status_code_path = join(output_path, "status_code")
     stdout_remote = join(output_path, "stdout")
@@ -59,21 +59,38 @@ def run_command(cmd: str) -> None:
             f.write(str(exitcode))
 
 
+def dispatch(idx: int, cmd: str) -> subprocess.Popen:
+    env = os.environ.copy()
+    output_path = os.environ["SATURN_RUN_REMOTE_OUTPUT_PATH"]
+    local_results_dir = os.environ["SATURN_RUN_LOCAL_RESULTS_DIR"]
+    output_path = join(output_path, str(idx))
+    local_results_dir = join(local_results_dir, str(idx))
+    env["SATURN_RUN_REMOTE_OUTPUT_PATH"] = output_path
+    env["SATURN_RUN_LOCAL_RESULTS_DIR"] = local_results_dir
+    proc = subprocess.Popen(
+        f"sc run {cmd}", env=env, stdout=sys.stdout, stderr=sys.stderr, shell=True
+    )
+    return proc
+
+
 def batch(cmds: List[str]) -> None:
-    procs = []
-    for idx, cmd in enumerate(cmds):
-        env = os.environ.copy()
-        output_path = os.environ["SATURN_RUN_REMOTE_OUTPUT_PATH"]
-        local_results_dir = os.environ["SATURN_RUN_LOCAL_RESULTS_DIR"]
-        output_path = join(output_path, str(idx))
-        local_results_dir = join(local_results_dir, str(idx))
-        env["SATURN_RUN_REMOTE_OUTPUT_PATH"] = output_path
-        env["SATURN_RUN_LOCAL_RESULTS_DIR"] = local_results_dir
-        proc = subprocess.Popen(
-            f"sc run {cmd}", env=env, stdout=sys.stdout, stderr=sys.stderr, shell=True
-        )
-        procs.append(proc)
-    print(procs)
-    for p in procs:
-        code = p.wait()
-        print(code)
+    nprocs = int(os.environ["SATURN_RUN_NPROCS"])
+    queue = list(enumerate(cmds))
+    running = {}
+    while True:
+        if len(queue) == 0 and len(running) == 0:
+            break
+        if len(running) < nprocs and len(queue) > 0:
+            idx, cmd = queue.pop(0)
+            proc = dispatch(idx, cmd)
+            running[idx] = proc
+        completed_idx = []
+        for idx, proc in running.items():
+            code = proc.poll()
+            if code is None:
+                continue
+            else:
+                completed_idx.append(idx)
+        for idx in completed_idx:
+            running.pop(idx)
+        time.sleep(1)
